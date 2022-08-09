c1c58a8ddae09baaf91536ab739a02d9a905cf4d
Vincent van Hees <v.vanhees@accelting.com>
2022-08-09 09:59:13 +0200

    adding examples to documentation and expanding unit test for Axivity data

--- tests/testthat/test_readAxivity.R
+++ tests/testthat/test_readAxivity.R
@@ -10,4 +10,14 @@ test_that("readAxivity reads data from file correctly", {
   expect_equal(AX3$data$time[5], 1551174909)
   expect_equal(AX3$data$temp[3], 18.65)
   expect_equal(floor(sum(abs(AX3$data[,2:4]))), 1407)
+  
+  # ask for more data then there is in the file
+  AX3b = readAxivity(filename = cwafile, desiredtz = "Europe/Berlin", start = 1, end = 1000)
+  expect_equal(AX3b$header$device, "Axivity")
+  expect_equal(nrow(AX3b$data), 17299)
+  expect_equal(ncol(AX3b$data), 7)
+  expect_equal(AX3b$data$time[5], 1551174909)
+  expect_equal(AX3b$data$temp[3], 18.65)
+  expect_equal(floor(sum(abs(AX3b$data[,2:4]))), 24873)
+  
 })
