2caedffa1dc677b87f933a6c26767381a45de12b
Vincent van Hees <v.vanhees@accelting.com>
2022-09-29 14:41:00 +0200

    adding zzz file to check package version on attach

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(...) {
+  if (!interactive()) return()
+  pkgs <- available.packages()
+  cran_version <- package_version(pkgs[which(pkgs[,1] == "GGIRread"),"Version"])
+  local_version <- utils::packageVersion("GGIRread")
+  behind_cran <- cran_version > local_version
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- paste0("A newer version of GGIRread is available with bug fixes and new features. [", local_version," --> ", cran_version, "]")
+      packageStartupMessage(msg)
+    }   
+  }
+}
