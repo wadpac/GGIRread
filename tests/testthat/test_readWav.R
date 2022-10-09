8e1cda00430900b2d930cdec8b8244913f02f27f
Vincent van Hees <v.vanhees@accelting.com>
2022-10-09 14:18:28 +0200

    trying to make wavRead test more robust

--- tests/testthat/test_readWav.R
+++ tests/testthat/test_readWav.R
@@ -5,6 +5,6 @@ test_that("readWav reads data in file correctly", {
   WAV = expect_warning(readWav(filename = wavfile, units = "samples", start = 1, end = 100))
   expect_equal(nrow(WAV$rawxyz), 100)
   expect_equal(round(sum(WAV$rawxyz),digits = 2), -97.08)
-  expect_equal(as.character(WAV$header$hnames[10]), "Channel-1")
-  expect_equal(as.character(WAV$header$hvalues[11]), "8")
+  expect_equal(as.character(WAV$header$hvalues[which(as.character(WAV$header$hnames) == "Channel-1")]), "Accel-X")
+  expect_equal(as.character(WAV$header$hvalues[which(as.character(WAV$header$hnames) == "Scale-1")]), "8")
 })
