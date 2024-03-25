7325e2460846ff3bf6836316b18582294a851562
Vincent van Hees <v.vanhees@accelting.com>
2024-03-25 17:25:07 +0100

    increment time correctly between pages, fixes #61

--- R/readGENEActiv.R
+++ R/readGENEActiv.R
@@ -70,7 +70,9 @@ readGENEActiv = function(filename, start = 0, end = 0, progress_bar = FALSE,
   }
   
   # Correct timestamps
-  page_offset = (((start - 1) * 300) / rawdata$info$SampleRate)
+  if (start > 0) {
+    page_offset = (((start - 1) * 300) / rawdata$info$SampleRate)
+  }
   starttime_num = as.numeric(starttime_posix) + page_offset
   rawdata$time = rawdata$time + abs(rawdata$time[1]) + starttime_num
   return(invisible(list(
