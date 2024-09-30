3eab46021ff6e962fae6ed4c95067f57082b6e86
Vincent van Hees <v.vanhees@accelting.com>
2024-09-30 11:30:49 +0200

    simplify PHB functionality to just read and merge #68

--- R/mergePHBfilePairs.R
+++ R/mergePHBfilePairs.R
@@ -1,57 +1,40 @@
-mergePHBfilePairs = function(inputPath = ".", outputPath = ".",
+mergePHBfilePairs = function(filenames = NULL,
                              timeformat = "%m/%d/%Y %H:%M:%S",
                              desiredtz = "", configtz = NULL,
                              timeformatName = "timeformat") {
   # merges Philips Health Band xlsx files per participant
   # as there can be multiple files per participant.
-  fnames = dir(inputPath, recursive = FALSE, full.names = TRUE, pattern = "[.]xlsx")
-  fileOverview = data.frame(filename = fnames)
-  extractID = function(x) {
-    x = basename(x)
-    x = gsub(pattern = "sleep_wake", replacement = "sleepwake", x = tolower(x))
-    ID = unlist(strsplit(x, "_"))[2]
-    return(ID)
+  if (length(filenames) != 2) {
+    stop("Provide two filenames")
   }
-  fileOverview$ID = unlist(lapply(fileOverview$filename, FUN = extractID))
   
-  uids = unique(fileOverview$ID)
-  for (uid in uids) {
-    filesForThisPerson = fileOverview$filename[which(fileOverview$ID == uid)]
-    # Identify both file
-    file1 = grep(pattern = "datalist", x = filesForThisPerson, ignore.case = TRUE)
-    file2 = grep(pattern = "sleep_wake", x = filesForThisPerson, ignore.case = TRUE)
-    if (length(file1) == 0 && length(file2) == 0) {
-      next
-    }
-    # Data
-    deviceSN = NULL
-    if (length(file1) > 0) {
-      data1 = readPHBCount(filename = filesForThisPerson[file1], timeformat = timeformat,
-                   desiredtz = desiredtz, configtz = configtz,
-                   timeformatName = timeformatName)
-      deviceSN = data1$deviceSN
-    }
-    # Sleep wake scores
-    if (length(file2) > 0) {
-      data2 = readPHBCount(filename = filesForThisPerson[file2], timeformat = timeformat,
-                           desiredtz = desiredtz, configtz = configtz,
-                           timeformatName = timeformatName)
-    }
-    if (length(file1) > 0 && length(file2) > 0) {
-      data2$data = data2$data[, which(colnames(data2$data) != "sleepEventMarker")]
-      data = merge(data1$data, data2$data, by = "timestamp")
-    } else {
-      if (length(file1) > 0) {
-        data = data1$data
-      } else {
-        data = data2$data
-      }
-    }
-    colnames(data)[grep(pattern = "timestamp", x = colnames(data))] = "timestamp"
-    newName = gsub(pattern = "Sleep_Wake", replacement = "def", x =  basename(filesForThisPerson[file2]), ignore.case = TRUE)
-    newName = paste0(unlist(strsplit(newName, "[.]")) , collapse = paste0("_", deviceSN, "."))
-    newName = gsub(pattern = "xlsx", replacement = "csv", x = newName)
-    outputfile = paste0(outputPath, "/", newName)
-    write.csv(x = data, file = outputfile, row.names = FALSE)
+  # Identify both file
+  file1 = grep(pattern = "datalist", x = filenames, ignore.case = TRUE)
+  file2 = grep(pattern = "sleep_wake", x = filenames, ignore.case = TRUE)
+
+  # Datalist file (with all variables except sleep/wake scores)
+  deviceSN = NULL
+  if (length(file1) > 0) {
+    data1 = readPHBCount(filename = filenames[file1], timeformat = timeformat,
+                         desiredtz = desiredtz, configtz = configtz,
+                         timeformatName = timeformatName)
+    deviceSN = data1$deviceSN
   }
+  # Sleep wake scores file
+  if (length(file2) > 0) {
+    data2 = readPHBCount(filename = filenames[file2], timeformat = timeformat,
+                         desiredtz = desiredtz, configtz = configtz,
+                         timeformatName = timeformatName)
+  }
+  if (length(file1) > 0 && length(file2) > 0) {
+    data2$data = data2$data[, which(colnames(data2$data) != "sleepEventMarker")]
+    data = merge(data1$data, data2$data, by = "timestamp")
+  } else {
+    if (length(file1) > 0) {
+      data = data1$data
+    } else {
+      data = data2$data
+    }
+  }
+  invisible(list(data = data, deviceSN = deviceSN))
 }
--- tests/testthat/test_mergePHBfilePairs.R
+++ tests/testthat/test_mergePHBfilePairs.R
@@ -4,32 +4,15 @@ test_that("merging of PHB files goes correctly", {
   # prepare folder with test files
   file1 = system.file("testfiles/DataList_AH1234567890_PhilipsHealthBand.xlsx", package = "GGIRread")
   file2 = system.file("testfiles/Sleep_Wake_AH1234567890_PhilipsHealthBand.xlsx", package = "GGIRread")
-  path = "./phb_test_folder"
-  if (!dir.exists(path)) {
-    dir.create(path)
-  }
-  invisible(file.copy(from = file1, to = path, overwrite = TRUE))
-  invisible(file.copy(from = file2, to = path, overwrite = TRUE))
-  
   # apply function to merge the files
-  mergePHBfilePairs(inputPath = path, outputPath = path,
+  D = mergePHBfilePairs(filenames = c(file1, file2),
                     timeformat = "%m-%d-%Y %H:%M:%S",
                     desiredtz =  "Europe/Amsterdam")
-  
-  # check whether merged file exists
-  newfiles = dir(path, full.names = TRUE)
-  newFile = newfiles[grep(pattern = "def_AH1234567890_PhilipsHealthBand_000100621938.csv", x = newfiles)]
-  expect_true(length(newFile) == 1)
-  
-  # check file content
-  data = read.csv(newFile)
-  expect_equal(nrow(data), 246)
-  expect_equal(ncol(data), 19)
-  expect_equal(sum(data$counts), 50898)
-  expect_equal(sum(data$steps), 1)
-  expect_equal(data$timestamp[1], "2022-11-05 01:10:00")
-  
-  # clean up
-  if (dir.exists(path))  unlink(path, recursive = TRUE)
+  expect_equal(nrow(D$data), 246)
+  expect_equal(ncol(D$data), 19)
+  expect_equal(sum(D$data$counts), 50898)
+  expect_equal(sum(D$data$steps), 1)
+  expect_equal(format(D$data$timestamp[1]), "2022-11-05 01:10:00")
+  expect_equal(D$deviceSN, "000100621938")
 })
  
