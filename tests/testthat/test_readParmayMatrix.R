23cfce20e42383104c50998ae2eab7a3505dec62
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2025-03-28 11:20:24 +0100

    exted Parmay Matrix tests with gyroscope and corrupted packet in new file

--- tests/testthat/test_readParmayMatrix.R
+++ tests/testthat/test_readParmayMatrix.R
@@ -41,3 +41,37 @@ test_that("File including accelerometer, heart rate, and temperature data at 100
                          read_heart = TRUE)
   expect_equal(ncol(BIN$data), 9) # time, x, y, z, body temp, ambient temp, hr_raw, hr, remarks
 })
+
+
+test_that("File including accelerometer and gyroscope", {
+  binfile  = system.file("testfiles/mtx_12.5Hz_acc_gyro.BIN", package = "GGIRread")[1]
+  BIN = readParmayMatrix(filename = binfile, desiredtz = "Europe/Berlin", start = 1, end = NULL)
+  expect_equal(BIN$header$sf, 12.5)
+  expect_equal(BIN$header$acc_dynrange, 8)
+  expect_equal(as.numeric(BIN$header$starttime), BIN$data$time[1])
+  expect_equal(nrow(BIN$data), 1425)
+  expect_equal(ncol(BIN$data), 7) # time, x, y, z, body temp, ambient temp,remarks
+  expect_equal(nrow(BIN$QClog), 1) # 1 packet in file
+  expect_true(all(BIN$QClog$checksum_pass))
+  BIN = readParmayMatrix(filename = binfile, desiredtz = "Europe/Berlin", start = 1, end = NULL,
+                         read_gyro = TRUE)
+  expect_equal(ncol(BIN$data), 10) # time, x, y, z, gyro (x, y, z) body temp, ambient temp, hr_raw, hr, remarks
+})
+
+
+test_that("Identification of corrupt packets (1st packet is corrupted)", {
+  binfile  = system.file("testfiles/mtx_corrupted.BIN", package = "GGIRread")[1]
+  BIN = readParmayMatrix(filename = binfile, desiredtz = "Europe/Berlin", start = 1, end = NULL)
+  expect_equal(BIN$QClog$checksum_pass, c(F, T, T, T))
+  expect_equal(BIN$header$sf, 100)
+  expect_equal(BIN$header$acc_dynrange, 8)
+  expect_equal(as.numeric(BIN$header$starttime), BIN$data$time[1])
+  expect_equal(nrow(BIN$data), 39400)
+  # in corrupt packet (#1), acc data should be 0,0,1, and temperature should be NA
+  # check this in first row
+  expect_equal(BIN$data$acc_x[1], 0)
+  expect_equal(BIN$data$acc_y[1], 0)
+  expect_equal(BIN$data$acc_z[1], 1)
+  expect_true(is.na(BIN$data$bodySurface_temp[1]))
+  expect_true(is.na(BIN$data$ambient_temp[1]))
+})
